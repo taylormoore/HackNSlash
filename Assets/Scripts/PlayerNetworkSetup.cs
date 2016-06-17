94abab357a511d5cb1bc23223fbc6cdd1218b0a5
Santos Tobar <zacktobar13@berkeley.edu>
2016-06-16 20:46:19 -0700

    Beginning of networking.

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	   if (isLocalPlayer) {
+            GetComponent<PlayerMovement>().enabled = true;
+            GetComponent<PlayerInput>().enabled = true;
+       }
+	}
+}
