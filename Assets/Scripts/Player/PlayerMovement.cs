a62f8caf04581f86a7375b58451d3783bea5bd6d
Zack Tobar <zacktobar13@gmail.com>
2016-06-28 19:18:41 -0700

    still testing sprite flipping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,33 +4,10 @@ using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour {
 
-	public Sprite facingUp, facingDown, facingLeft, facingRight;
 	public float movementSpeed;
-	[SyncVar] SpriteRenderer spriteRenderer;
-
-	void Awake() {
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		CmdChangeSprite(facingDown);
-	}
-
 
 	void Update() {
-		if (PlayerInput.horizontalAxis > .35f) {
-			CmdChangeSprite(facingRight);
-		} else if (PlayerInput.horizontalAxis < -.35f) {
-			CmdChangeSprite(facingLeft);
-		}
-		if (PlayerInput.verticalAxis > .35f) {
-			CmdChangeSprite(facingUp);
-		} else if (PlayerInput.verticalAxis < -.35f) {
-			CmdChangeSprite(facingDown);
-		}
 		transform.Translate(Vector2.up * Time.deltaTime * PlayerInput.verticalAxis * movementSpeed);
 		transform.Translate(Vector2.right * Time.deltaTime * PlayerInput.horizontalAxis * movementSpeed);
-	}
-	
-	[Command]
-	private void CmdChangeSprite(Sprite newSprite) {
-		spriteRenderer.sprite = newSprite;
 	}
 }
