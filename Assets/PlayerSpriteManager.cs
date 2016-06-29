33b4317842366219302195a0a30d37fa9fdc1b15
Zack Tobar <zacktobar13@gmail.com>
2016-06-28 19:20:43 -0700

    forgot files

---
+++ Assets/PlayerSpriteManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSpriteManager : NetworkBehaviour {
+
+	public Sprite facingUp, facingDown, facingLeft, facingRight;
+	[SyncVar] SpriteRenderer spriteRenderer;
+
+	void Awake() {
+		if (isLocalPlayer) {
+			spriteRenderer = GetComponent<SpriteRenderer>();
+			CmdChangeSprite(facingDown);
+		}
+	}
+
+	void Update() {
+		if (PlayerInput.horizontalAxis > .35f) {
+			CmdChangeSprite(facingRight);
+		} else if (PlayerInput.horizontalAxis < -.35f) {
+			CmdChangeSprite(facingLeft);
+		}
+		if (PlayerInput.verticalAxis > .35f) {
+			CmdChangeSprite(facingUp);
+		} else if (PlayerInput.verticalAxis < -.35f) {
+			CmdChangeSprite(facingDown);
+		}
+	}
+
+	[Command]
+	private void CmdChangeSprite(Sprite newSprite) {
+		if (spriteRenderer == null)
+			return;
+
+		spriteRenderer.sprite = newSprite;
+	}
+}
