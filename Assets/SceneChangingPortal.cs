0b53749f64434b6d39fba924bb8efcdf1857ce74
Zack Tobar <zacktobar13@gmail.com>
2016-06-28 21:58:25 -0700

    Still have error when client goes through portal, but we're making progress

--- Assets/SceneChangingPortal.cs
+++ Assets/SceneChangingPortal.cs
@@ -6,11 +6,11 @@ public class SceneChangingPortal : MonoBehaviour {
 	public string nextSceneName;
 
 	public void OnTriggerEnter2D(Collider2D other) {
-		nextSceneName = "Scenes/" + nextSceneName;
+		string nextScene = "Scenes/" + nextSceneName;
 		if (other.gameObject.tag == "Player") {
 			GameObject.FindWithTag("NetworkManager")
 				.GetComponent<NetworkManager>()
-				.ServerChangeScene(nextSceneName);
+				.ServerChangeScene(nextScene);
 		}
 	}
 }
