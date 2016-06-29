7e43854e8a80948115995641ebac47f80c5e2851
Taylor Moore <taylorjmoore92@gmail.com>
2016-06-28 22:08:48 -0500

    Converted animations to use animator component

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,34 +1,34 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour {
 
-	public Sprite facingUp, facingDown, facingLeft, facingRight;
-	public float movementSpeed;
-	SpriteRenderer spriteRenderer;
+    public Sprite facingUp, facingDown, facingLeft, facingRight;
+    public float movementSpeed;
+    Animator animator;
 
-	void Start() {
-		if (isLocalPlayer) {
-			spriteRenderer = GetComponent<SpriteRenderer>();
-			Debug.Log("Here, Sprite Renderer: " + spriteRenderer);
-		}
-	}
+    void Start() {
+        if (isLocalPlayer) {
+            animator = GetComponent<Animator>();
+        }
+    }
 
-	void Update() {
-		if (isLocalPlayer && spriteRenderer != null) {
-			if (PlayerInput.horizontalAxis > .35f) {
-				spriteRenderer.sprite = facingRight;
-			} else if (PlayerInput.horizontalAxis < -.35f) {
-				spriteRenderer.sprite = facingLeft;
-			}
-			if (PlayerInput.verticalAxis > .35f) {
-				spriteRenderer.sprite = facingUp;
-			} else if (PlayerInput.verticalAxis < -.35f) {
-				spriteRenderer.sprite = facingDown;
-			}
-		}
-		transform.Translate(Vector2.up * Time.deltaTime * PlayerInput.verticalAxis * movementSpeed);
-		transform.Translate(Vector2.right * Time.deltaTime * PlayerInput.horizontalAxis * movementSpeed);
-	}
+    void Update() {
+        if (isLocalPlayer && animator != null) {
+            if (PlayerInput.horizontalAxis > .35f) {
+                animator.SetInteger("direction", 1);
+            }
+            else if (PlayerInput.horizontalAxis < -.35f) {
+                animator.SetInteger("direction", 0);
+            }
+            if (PlayerInput.verticalAxis > .35f) {
+                animator.SetInteger("direction", 2);
+            }
+            else if (PlayerInput.verticalAxis < -.35f) {
+                animator.SetInteger("direction", 3);
+            }
+        }
+        transform.Translate(Vector2.up * Time.deltaTime * PlayerInput.verticalAxis * movementSpeed);
+        transform.Translate(Vector2.right * Time.deltaTime * PlayerInput.horizontalAxis * movementSpeed);
+    }
 }
