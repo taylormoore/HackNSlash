4134efa84899f5cf91d261c9dd1d72a32b3c36a1
Zack Tobar <zacktobar13@gmail.com>
2016-06-29 17:52:15 -0700

    Portal now destroys itself

--- Assets/Scripts/Player/PortalAbility.cs
+++ Assets/Scripts/Player/PortalAbility.cs
@@ -4,49 +4,50 @@ using UnityEngine.SceneManagement;
 
 public class PortalAbility : NetworkBehaviour {
 
-    [SerializeField]
-    GameObject portal;
+	[SerializeField]
+	GameObject portal;
 
-    float lastPortalCast;
-    float portalCooldown;
+	GameObject myPortal;
 
-    void Start() {
-        lastPortalCast = Time.time;
-        portalCooldown = 3f;
-    }
+	float lastPortalCast;
+	float portalCooldown;
 
-    void Update() {
-        if (PortalInput.castPortal) {
-            CmdCastPortal();
-        }
-    }
+	void Start() {
+		lastPortalCast = Time.time;
+		portalCooldown = 3f;
+	}
+
+	void Update() {
+		if (PortalInput.castPortal) {
+			CmdCastPortal();
+		}
+	}
 
 
-    // Creates portal if we are not in hub world and if cooldown is up.
-    [Command]
-    void CmdCastPortal() {
-        if (SceneManager.GetActiveScene().name == "HubWorld") {
-            Debug.Log("Cannot cast portal in hub world");
-        }
-        else {
-            if (Time.time > lastPortalCast + portalCooldown) {
-                GameObject myPortal = Instantiate(portal,
-                                                  new Vector2(transform.position.x,
-                                                  transform.position.y + 4),
-                                                  transform.rotation) as GameObject;
-                lastPortalCast = Time.time;
-                NetworkServer.Spawn(myPortal);
-            }
-            else {
-                Debug.Log("Portal is on cooldown.");
-            }
-        }
-    }
+	// Creates portal if we are not in hub world and if cooldown is up.
+	[Command]
+	void CmdCastPortal() {
+		if (SceneManager.GetActiveScene().name == "HubWorld") {
+			Debug.Log("Cannot cast portal in hub world");
+			return;
+		}
+		if (Time.time > lastPortalCast + portalCooldown) {
+			if (myPortal != null) {
+				Destroy(myPortal);
+			}
+			myPortal = Instantiate(portal, new Vector2(transform.position.x,
+				transform.position.y + 4), transform.rotation) as GameObject;
+			lastPortalCast = Time.time;
+			NetworkServer.Spawn(myPortal);
+		} else {
+			Debug.Log("Portal is on cooldown.");
+		}
+	}
 
-    [Command]
-    void CmdRemovePreviousPortal() {
-        //TO DO
-    }
+	[Command]
+	void CmdRemovePreviousPortal() {
+		//TO DO
+	}
 }
 
 
