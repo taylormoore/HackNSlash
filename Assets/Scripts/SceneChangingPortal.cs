f8381f13a021bd79a42059888e4380a69918ee4b
Zack Tobar <zacktobar13@gmail.com>
2016-07-08 00:23:14 -0700

    scene changing attempt 100

--- Assets/Scripts/SceneChangingPortal.cs
+++ Assets/Scripts/SceneChangingPortal.cs
@@ -7,25 +7,19 @@ public class SceneChangingPortal : NetworkBehaviour {
     public string nextSceneName;
 
     public void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Player") {
-			GameObject[] players = PlayerReference.GetPlayers().ToArray();
-			CmdChangeScene();
-			//for (int i = 0; i < NetworkServer.connections.Count; i++) {
-			//	NetworkServer.SetClientReady(NetworkServer.connections[i]);
-			//}
+		if (other.gameObject.tag != "Player") {
+			return;
 		}
-    }
-
-	void OnLeveLWasLoaded(int level) {
 		string nextScene = "Scenes/" + nextSceneName;
 		GameObject.FindWithTag("NetworkManager")
 				.GetComponent<NetworkManager>()
 				.ServerChangeScene(nextScene);
+		//for (int i = 0; i < NetworkServer.connections.Count; i++) {
+		//	NetworkServer.SetClientReady(NetworkServer.connections[i]);
+		//}
 	}
 
-	[Command]
-	void CmdChangeScene() {
-		string nextScene = "Scenes/" + nextSceneName;
-		SceneManager.LoadScene(nextScene);
+	void OnLeveLWasLoaded(int level) {
+		Debug.Log("Here");
 	}
 }
