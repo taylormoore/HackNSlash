d523e47101c0fd7cae9fa19ab3e7ed0a85f28890
Taylor Moore <taylorjmoore92@gmail.com>
2016-07-31 03:15:08 -0500

    Back to rigid body from character controllers.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,44 +4,40 @@ using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour {
 
-    public Sprite facingUp, facingDown, facingLeft, facingRight;
-    public float movementSpeed;
-    Animator animator;
-    CharacterController controller;
-    private Player player;
-    private float horizontalAxis, verticalAxis = 0f;
+	public Sprite facingUp, facingDown, facingLeft, facingRight;
+	public float movementSpeed;
+	Animator animator;
+	private Player player;
+	private float horizontalAxis, verticalAxis = 0f;
 
-    void Start() {
-        if (isLocalPlayer) {
-            animator = GetComponent<Animator>();
-        }
-        controller = GetComponent<CharacterController>();
-        player = ReInput.players.GetPlayer(0);
-    }
+	void Start() {
+		if (isLocalPlayer) {
+			animator = GetComponent<Animator>();
+		}
+		player = ReInput.players.GetPlayer(0);
+	}
 
-    void Update() {
-        DetectMovementInput();
+	void Update() {
+		DetectMovementInput();
 
-        if (isLocalPlayer && animator != null) {
-            if (horizontalAxis > .35f) {
-                animator.SetInteger("direction", 1);
-            }
-            else if (horizontalAxis < -.35f) {
-                animator.SetInteger("direction", 0);
-            }
-            if (verticalAxis > .35f) {
-                animator.SetInteger("direction", 2);
-            }
-            else if (verticalAxis < -.35f) {
-                animator.SetInteger("direction", 3);
-            }
-        }
-        controller.Move(Vector2.up * Time.deltaTime * verticalAxis * movementSpeed);
-        controller.Move(Vector2.right * Time.deltaTime * horizontalAxis * movementSpeed);
-    }
+		if (isLocalPlayer && animator != null) {
+			if (horizontalAxis > .35f) {
+				animator.SetInteger("direction", 1);
+			} else if (horizontalAxis < -.35f) {
+				animator.SetInteger("direction", 0);
+			}
+			if (verticalAxis > .35f) {
+				animator.SetInteger("direction", 2);
+			} else if (verticalAxis < -.35f) {
+				animator.SetInteger("direction", 3);
+			}
+		}
+		transform.Translate(Vector2.up * Time.deltaTime * verticalAxis * movementSpeed);
+		transform.Translate(Vector2.right * Time.deltaTime * horizontalAxis * movementSpeed);
+	}
 
-    void DetectMovementInput() {
-        horizontalAxis = player.GetAxisRaw("MoveHorizontal");
-        verticalAxis = player.GetAxisRaw("MoveVertical");
-    }
+	void DetectMovementInput() {
+		horizontalAxis = player.GetAxisRaw("MoveHorizontal");
+		verticalAxis = player.GetAxisRaw("MoveVertical");
+	}
 }
--- Assets/Scripts/Player/PlayerNetworkSetup.cs
+++ Assets/Scripts/Player/PlayerNetworkSetup.cs
@@ -3,20 +3,19 @@ using UnityEngine.Networking;
 
 public class PlayerNetworkSetup : NetworkBehaviour {
 
-    void Start() {
-        if (isLocalPlayer) {
-            GetComponent<PlayerMovement>().enabled = true;
-            GetComponent<PlayerBasicAttack>().enabled = true;
-            GetComponent<PortalAbility>().enabled = true;
-            // GetComponent<CharacterController>().enabled = true;
-            Camera2DFollow camera = Camera.main.GetComponent<Camera2DFollow>();
-            camera.enabled = true;
-            camera.target = gameObject.transform;
-        }
-        PlayerReference.AddPlayer(gameObject);
-    }
+	void Start() {
+		if (isLocalPlayer) {
+			GetComponent<PlayerMovement>().enabled = true;
+			GetComponent<PlayerBasicAttack>().enabled = true;
+			GetComponent<PortalAbility>().enabled = true;
+			Camera2DFollow camera = Camera.main.GetComponent<Camera2DFollow>();
+			camera.enabled = true;
+			camera.target = gameObject.transform;
+		}
+		PlayerReference.AddPlayer(gameObject);
+	}
 
-    public void SceneChange() {
+	public void SceneChange() {
 
-    }
+	}
 }
