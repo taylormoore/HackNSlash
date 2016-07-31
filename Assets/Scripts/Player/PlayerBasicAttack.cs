1664e68605ec7fb0d1c10420ff24d64a7c3a7a15
Zack Tobar <Zacktobar13@gmail.com>
2016-07-30 23:38:02 -0700

    code needs to be cleaned up, but it works for 2 player

--- Assets/Scripts/Player/PlayerBasicAttack.cs
+++ Assets/Scripts/Player/PlayerBasicAttack.cs
@@ -4,14 +4,17 @@ using UnityEngine.Networking;
 
 public class PlayerBasicAttack : NetworkBehaviour {
     public GameObject projectile;
+    public GameObject thisProjectile;
     float attackCooldown = .4f;
     float lastAttack = 0f;
+    PlayerBasicAttack thisScript;
 
     private Player player;
     private bool attackRight, attackLeft, attackUp, attackDown = false;
 
     void Start() {
         player = ReInput.players.GetPlayer(0);
+        thisScript = gameObject.GetComponent<PlayerBasicAttack>();
     }
 
     void Update() {
@@ -19,30 +22,50 @@ public class PlayerBasicAttack : NetworkBehaviour {
 
         if (Time.time > lastAttack + attackCooldown) {
             if (attackLeft) {
-                CmdShootProjectile(1);
+                //((GameObject)Instantiate(projectile, transform.position, transform.rotation)).SendMessage("UpdateDirection", 1);
+                CmdShootProjectile(1, gameObject);
                 lastAttack = Time.time;
             }
             else if (attackRight) {
-                CmdShootProjectile(2);
+                //((GameObject)Instantiate(projectile, transform.position, transform.rotation)).SendMessage("UpdateDirection", 1);
+                CmdShootProjectile(2, gameObject);
                 lastAttack = Time.time;
             }
             else if (attackUp) {
-                CmdShootProjectile(3);
+                //((GameObject)Instantiate(projectile, transform.position, transform.rotation)).SendMessage("UpdateDirection", 1);
+                CmdShootProjectile(3, gameObject);
                 lastAttack = Time.time;
             }
             else if (attackDown) {
-                CmdShootProjectile(4);
+                
+                //((GameObject)Instantiate(projectile, transform.position, transform.rotation)).SendMessage("UpdateDirection", 1);
+                CmdShootProjectile(4, gameObject);
                 lastAttack = Time.time;
             }
         }
     }
 
+    public void SetProjectileToDestroy(GameObject proj)
+    {
+        thisProjectile = proj;
+    }
+
     [Command]
-    public void CmdShootProjectile(int direction) {
-        GameObject thisProjectile = (GameObject) Instantiate(projectile, transform.position, transform.rotation);
-        thisProjectile.SendMessage("UpdateDirection", direction);
-        Destroy(thisProjectile, 3f);
-        NetworkServer.Spawn(thisProjectile);
+    public void CmdShootProjectile(int direction, GameObject spawner) {
+        RpcDestroyProjectile(spawner.transform.position, direction, spawner);
+        //thisProjectile = (GameObject) Instantiate(projectile, transform.position, transform.rotation);
+        //thisProjectile.SendMessage("UpdateDirection", direction);
+        //Destroy(thisProjectile, 3f);
+        //NetworkServer.Spawn(thisProjectile);
+    }
+
+    [ClientRpc]
+    public void RpcDestroyProjectile(Vector2 position, int direction, GameObject spawner)
+    {
+        if (gameObject == spawner)
+            ((GameObject)Instantiate(projectile, transform.position, Quaternion.identity)).SendMessage("UpdateDirection", direction);
+        else
+            ((GameObject)Instantiate(projectile, spawner.transform.position, Quaternion.identity)).SendMessage("UpdateDirection", direction);
     }
 
     void DetectAttackInput() {
