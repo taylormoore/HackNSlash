bbf91507979e9fbed9f9f7fd794c2023e0d15f89
Zack Tobar <Zacktobar13@gmail.com>
2016-07-31 16:05:11 -0700

    Fixed bug where player could use portal ability in hub world after leaving and reentering hubworld

--- Assets/Scripts/Player/PortalAbility.cs
+++ Assets/Scripts/Player/PortalAbility.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class PortalAbility : NetworkBehaviour {
 
@@ -9,19 +10,23 @@ public class PortalAbility : NetworkBehaviour {
     GameObject portal;
 
     GameObject myPortal;
+	List<string> noPortalScenes = new List<string>();
     private Player player;
 
     float lastPortalCast;
+	[SerializeField]
     float portalCooldown;
 
     void Start() {
-        lastPortalCast = Time.time;
-        portalCooldown = 3f;
         player = ReInput.players.GetPlayer(0);
+        lastPortalCast = Time.time;
+		noPortalScenes.Add("HubWorld");
+		noPortalScenes.Add("Scenes/HubWorld");
     }
 
     void Update() {
-        if (player.GetButtonDown("Portal")) {
+        if (player.GetButtonDown("Portal") && Time.time > lastPortalCast + portalCooldown
+			&& !noPortalScenes.Contains(SceneManager.GetActiveScene().name)) {
             CmdCastPortal();
         }
     }
@@ -30,22 +35,13 @@ public class PortalAbility : NetworkBehaviour {
     // Creates portal if we are not in hub world  and if cooldown is up.
     [Command]
     void CmdCastPortal() {
-        if (SceneManager.GetActiveScene().name == "HubWorld") {
-            Debug.Log("Cannot cast portal in hub world");
-            return;
-        }
-        if (Time.time > lastPortalCast + portalCooldown) {
-            if (myPortal != null) {
-                Destroy(myPortal);
-            }
-            myPortal = Instantiate(portal, new Vector2(transform.position.x,
-                transform.position.y + 4), transform.rotation) as GameObject;
-            lastPortalCast = Time.time;
-            NetworkServer.Spawn(myPortal);
-        }
-        else {
-            Debug.Log("Portal is on cooldown.");
-        }
+		if (myPortal != null) {
+			Destroy(myPortal);
+		}
+		myPortal = Instantiate(portal, new Vector2(transform.position.x,
+			transform.position.y + 2), transform.rotation) as GameObject;
+		lastPortalCast = Time.time;
+		NetworkServer.Spawn(myPortal);
     }
 }
 
